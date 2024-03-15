8b29cff12b646454e3458bc415d79aacf024a3c2
Osmanosis <161318183+Osmanosis@users.noreply.github.com>
2024-03-15 12:24:06 +0000

    tagall

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,13 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`Group : *${groupMetadata.subject}*\n Members : *${participants.length}*${text ? `\nMessage : ${text}\n` : ''}\n *MENTIONS*\n` + users.map(v => '- @' + v.replace(/@.+/, '')).join`\n` + '\n\n *OSGenieᴮᴼᵀ*', null, {
+    m.reply(` 
+    *Group: ${groupMetadata.subject}*
+
+    ${text ? `*Message : ${text}*` : ''}
+
+*┏━━━━⊱ ◈ Mentions: ${participants.length} ◈ ⊰━━━━┓*\n\n`
+        + users.map(v => '⟫ @' + v.replace(/@.+/, '')).join`\n`
+        + '\n\n *┗━━━━━⊱ OSGenieᴮᴼᵀ  ⊰━━━━━┛*', null, {
         mentions: users
     })
 }
